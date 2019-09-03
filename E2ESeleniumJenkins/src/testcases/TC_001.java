0fe177e21ca98601bf55679a13e9a05a7d70042c
shysu <shysu@DESKTOP-L8RR3NI>
2019-09-03 17:39:55 +0530

    Updated test cases

--- E2ESeleniumJenkins/src/testcases/TC_001.java
+++ E2ESeleniumJenkins/src/testcases/TC_001.java
@@ -6,10 +6,11 @@ import org.testng.annotations.Test;
 public class TC_001 {
 	
 	@Test
-	public void tc1()
+	public void tc1() throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
+		Thread.sleep(10000);
 		driver.get("https://www.facebook.com");
 		driver.findElementById("email").sendKeys("Test1");
 		driver.findElementById("pass").sendKeys("test@1");
--- E2ESeleniumJenkins/src/testcases/TC_002.java
+++ E2ESeleniumJenkins/src/testcases/TC_002.java
@@ -6,10 +6,11 @@ import org.testng.annotations.Test;
 public class TC_002 {
 	
 	@Test
-	public void tc2()
+	public void tc2() throws InterruptedException
 	{
 		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
+		Thread.sleep(10000);
 		driver.get("https://www.facebook.com");
 		driver.findElementById("email").sendKeys("Test2");
 		driver.findElementById("pass").sendKeys("test@2");
