e268733746fc9724f44427bf6575cfe00d935f56
shysu <shysu@DESKTOP-L8RR3NI>
2019-09-03 18:33:36 +0530

    browser to Firefox

--- E2ESeleniumJenkins/src/testcases/TC_004.java
+++ E2ESeleniumJenkins/src/testcases/TC_004.java
@@ -1,6 +1,6 @@
 package testcases;
 
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class TC_004 {
@@ -8,8 +8,8 @@ public class TC_004 {
 	@Test
 	public void tc1() throws InterruptedException
 	{
-		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "./Driver/geckodriver.exe");
+		FirefoxDriver driver = new FirefoxDriver();
 		Thread.sleep(10000);
 		driver.get("https://www.facebook.com");
 		driver.findElementById("email").sendKeys("Test4");
